dd5adc39a9043bbe7b8fe1af7fff152e76faf413
Alexander White <whiteaj36@gmail.com>
2019-12-17 21:02:15 -0500

    add limit to match history

--- winrate/index.js
+++ winrate/index.js
@@ -28,8 +28,9 @@ const NAMES = {
 }
 
 const playerID = NAMES[process.argv[2]] || process.argv[2]
-
-fetch(`https://api.opendota.com/api/players/${playerID}/matches`)
+const limit = process.argv[3]
+const limitQueryParam = `${limit ? 'limit='+limit : null}`
+fetch(`https://api.opendota.com/api/players/${playerID}/matches?${limitQueryParam}`)
 .then(res => res.json())
 .then(jsonResult => {
     const startTimesWithWins = jsonResult.map(match => { return { matchId: match.match_id, startingHour: convertStartTimesToHourBegan(match.start_time), isWin: isWin(match.radiant_win, match.player_slot)}})
